69c31558b7a0026e13741c722bccc821cd68eda5
ilnar861 <ilnarkino@yandex.ru>
2025-07-28 18:14:07 +0300

    Added note type field

--- IlnarApp.Domain/Note/Note.cs
+++ IlnarApp.Domain/Note/Note.cs
@@ -7,6 +7,7 @@ public class Note : Entity
 {
 	public string? Title { get; set; }
 	public required string Text { get; set; }
+	public required NoteType NoteType { get; set; }
 	public DateTime Date { get; set; } = DateTime.Now.AddHours(3);
 	public Archive? Archive { get; set; }
 	public List<Tag> Tags { get; set; } = [];
---
+++ IlnarApp.Infrastructure/Migrations/20250728151306_AddNoteTypeField.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlnarApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNoteTypeField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "NoteTypeId",
+                table: "Note",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Note_NoteTypeId",
+                table: "Note",
+                column: "NoteTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Note_NoteType_NoteTypeId",
+                table: "Note",
+                column: "NoteTypeId",
+                principalTable: "NoteType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Note_NoteType_NoteTypeId",
+                table: "Note");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Note_NoteTypeId",
+                table: "Note");
+
+            migrationBuilder.DropColumn(
+                name: "NoteTypeId",
+                table: "Note");
+        }
+    }
+}
