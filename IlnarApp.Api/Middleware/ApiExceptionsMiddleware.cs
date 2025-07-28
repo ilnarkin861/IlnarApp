e1159c88b3a604adc810e68bfb87110882f117f2
ilnar861 <ilnarkino@yandex.ru>
2025-07-28 17:39:09 +0300

    Implemented middleware class

---
+++ IlnarApp.Api/Middleware/ApiExceptionsMiddleware.cs
@@ -0,0 +1,66 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using IlnarApp.Api.Exceptions;
+using IlnarApp.Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IlnarApp.Api.Middleware;
+
+public class ApiExceptionsMiddleware(RequestDelegate next, ILogger<ApiExceptionsMiddleware> logger)
+{
+	public async Task InvokeAsync(HttpContext context)
+	{
+		try
+		{
+			await next(context);
+		}
+		catch (Exception exception)
+		{
+			await HandleExceptionAsync(context, exception);
+		}
+	}
+	
+	private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+	{
+		context.Response.ContentType = "application/json";
+		context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+
+		var response = new DefaultResponse
+		{
+			Success = false
+		};
+
+		switch (exception)
+		{
+			case ApiException restException:
+				response.ErrorMessages.Add(restException.Message);
+				break;
+
+			case EntityNotFoundException entityNotFoundException:
+				response.ErrorMessages.Add(entityNotFoundException.Message);
+				break;
+                
+			case IOException:
+				response.ErrorMessages.Add("File system operation error");
+				logger.LogError(exception, exception.Message);
+				break;
+                
+			case DbUpdateException:
+				response.ErrorMessages.Add("Ошибка базы данных");
+				logger.LogError(exception, exception.Message);
+				break;
+                
+			default:
+				context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+				response.ErrorMessages.Add("Internal Server Error");
+				response.StatusCode = (int) HttpStatusCode.InternalServerError;
+				logger.LogError(exception, exception.Message);
+				break;
+		}
+            
+		var errorResponse = JsonSerializer.Serialize(response);
+            
+		await context.Response.WriteAsync(errorResponse, Encoding.UTF8);
+	}
+}
