454f1f38779d7dcedfa53a5d4fd0da5f7d97246f
ilnar861 <ilnarkino@yandex.ru>
2025-07-30 18:56:07 +0300

    Changed date time fields

--- IlnarApp.Domain/Archive/Archive.cs
+++ IlnarApp.Domain/Archive/Archive.cs
@@ -6,5 +6,5 @@ public class Archive : Entity
 {
 	public required string Name { get; set; }
 	public List<Note> Notes { get; set; } = [];
-	public DateTime CreatedAt { get; set; } = DateTime.Now.AddHours(3);
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow.AddHours(3);
 }
--- IlnarApp.Domain/Tag/Tag.cs
+++ IlnarApp.Domain/Tag/Tag.cs
@@ -7,5 +7,5 @@ public class Tag : Entity
 {
 	public required string Name { get; set; }
 	public List<Note> Notes { get; set; } = [];
-	public DateTime CreatedAt { get; set; } = DateTime.Now.AddHours(3);
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow.AddHours(3);
 }
