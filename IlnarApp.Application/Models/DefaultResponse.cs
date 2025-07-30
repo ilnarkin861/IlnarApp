33d1d58dbbd8cfcd10b2b541c202866da589b2a9
ilnar861 <ilnarkino@yandex.ru>
2025-07-30 16:36:48 +0300

    Removed status code

--- IlnarApp.Application/Models/DefaultResponse.cs
+++ IlnarApp.Application/Models/DefaultResponse.cs
@@ -4,6 +4,5 @@ namespace IlnarApp.Application.Models;
 public class DefaultResponse
 {
 	public bool Success { get; set; }
-	public int StatusCode { get; set; }
-	public List<string> ErrorMessages { get; set; } = [];
+	public List<string> Messages { get; set; } = [];
 }
