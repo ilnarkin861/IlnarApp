5234fb52dfcfad9501dc2b8e444f6dc5bbce3bf7
ilnar861 <ilnarkino@yandex.ru>
2025-07-31 15:22:02 +0300

    Changed field types

--- IlnarApp.Application/Models/NoteFilterRequest.cs
+++ IlnarApp.Application/Models/NoteFilterRequest.cs
@@ -1,17 +1,14 @@
 using IlnarApp.Domain;
-using IlnarApp.Domain.Archive;
-using IlnarApp.Domain.Note;
 using IlnarApp.Domain.Tag;
-
 
 namespace IlnarApp.Application.Models;
 
 
 public class NoteFilterRequest : IEntityFilter
 {
-	public NoteType? NoteType { get; set; }
+	public Guid? NoteTypeId { get; set; }
 	public int? Year { get; set; }
 	public int? Month { get; set; }
-	public Archive? Archive { get; set; }
-	public List<Tag>? Tags { get; set; }
+	public Guid? ArchiveId { get; set; }
+	public List<Guid>? TagIds { get; set; }
 }
