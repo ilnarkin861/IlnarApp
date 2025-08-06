b95843196f14c816d2de57dbd9dac4441f5053fb
ilnar861 <ilnarkino@yandex.ru>
2025-08-06 18:05:14 +0300

    Added Day field

--- IlnarApp.Application/Models/NoteFilterRequest.cs
+++ IlnarApp.Application/Models/NoteFilterRequest.cs
@@ -9,6 +9,7 @@ public class NoteFilterRequest : IEntityFilter
 	public Guid? NoteTypeId { get; set; }
 	public int? Year { get; set; }
 	public int? Month { get; set; }
+	public int? Day { get; set; }
 	public Guid? ArchiveId { get; set; }
 	public List<Guid>? TagIds { get; set; }
 }
