72b7b1c62c0491a4de221a9a11bbf52199d7db23
ilnar861 <ilnarkino@yandex.ru>
2025-08-07 11:25:14 +0300

    Implemented auth option class

---
+++ IlnarApp.Application/Options/AuthOptions.cs
@@ -0,0 +1,19 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IlnarApp.Application.Options;
+
+
+public class AuthOptions
+{
+	public required string Issuer { get; set; }
+	public required string Audience { get; set; }
+	public required string Secret { get; set; }
+	public int TokenLifetime { get; set; }
+
+	public SymmetricSecurityKey GetSymmetricSecurityKey()
+	{
+		return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Secret));
+	}
+	
+}
