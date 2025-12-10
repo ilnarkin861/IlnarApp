fd96bc7016e7eeeea849b971e20d6d7a3a6400c0
ilnarkin861 <ilnarkino@yandex.ru>
2025-12-10 20:52:00 +0300

    Implemented user controller

---
+++ IlnarApp.Api/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+using IlnarApp.Api.Actions;
+using IlnarApp.Application.Models;
+using IlnarApp.Application.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IlnarApp.Api.Controllers;
+
+
+
+public class UserController(IUserService userService) : BaseController
+{
+    [HttpPost]
+    [Route("register")]
+    [ValidationAction]
+    public async Task<IActionResult> UserRegister([FromBody] UserData userData)
+    {
+        await userService.SignUp(userData.Email, userData.Password, userData.PinCode);
+        
+        return Ok(new ResponseData{Success = true, Messages = ["Пользователь успешно добавлен"] });
+    }
+    
+}
