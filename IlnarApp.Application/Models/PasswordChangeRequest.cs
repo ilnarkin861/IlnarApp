488ee081e597de7c194d2d0725582b0959394f62
ilnarkin861 <ilnarkino@yandex.ru>
2025-12-23 18:20:41 +0300

    Created password change request model class

---
+++ IlnarApp.Application/Models/PasswordChangeRequest.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IlnarApp.Application.Models;
+
+
+public class PasswordChangeRequest
+{
+    [Required(ErrorMessage = "Пароль не должен быть пустым")]
+    public required string OldPassword { get; set; }
+    
+    [Required(ErrorMessage = "Пароль не должен быть пустым")]
+    [MinLength(8, ErrorMessage = "Пароль должен состоять как минимум из 8 символов")]
+    public required string NewPassword { get; set; }
+    
+    [Required(ErrorMessage = "Подтвердите пароль")]
+    [Compare("NewPassword", ErrorMessage = "Пароли не совпадают")]
+    public required string ConfirmedPassword { get; set; }
+}
