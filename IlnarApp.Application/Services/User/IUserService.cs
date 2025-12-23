896e22c4ee8cf49107dd53e7d5d99bb39c8738c2
ilnarkin861 <ilnarkino@yandex.ru>
2025-12-23 18:40:55 +0300

    Changed method signature

--- IlnarApp.Application/Services/User/IUserService.cs
+++ IlnarApp.Application/Services/User/IUserService.cs
@@ -1,3 +1,5 @@
+using IlnarApp.Domain.Identity;
+
 namespace IlnarApp.Application.Services.User;
 
 
@@ -6,7 +8,7 @@ public interface IUserService
 	Task<bool> SignUp(string email, string password, string pinCode);
 	Task<string> SignIn(string email, string password);
 	Task<bool> ChangePassword(string oldPassword, string newPassword);
-	Task<String> GetEmail();
+	Task<ApplicationUser> GetUserInfo();
 	Task<bool> ChangeEmail(string oldEmail, string newEmail);
 	Task<bool> ChangePinCode(string oldPinCode, string newPinCode);
 	Task<bool> CheckPinCode(string pinCode);
