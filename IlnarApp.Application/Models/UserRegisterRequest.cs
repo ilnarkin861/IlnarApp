2d356d96f1415933d4e001490fb7f08c06962787
ilnarkin861 <ilnarkino@yandex.ru>
2025-12-24 17:47:40 +0300

    Changed max lenght error message for pin code

--- IlnarApp.Application/Models/UserRegisterRequest.cs
+++ IlnarApp.Application/Models/UserRegisterRequest.cs
@@ -14,7 +14,7 @@ public class UserRegisterRequest
     
     [Required(ErrorMessage = "PIN-код не должен быть пустым")]
     [MinLength(4, ErrorMessage = "PIN-код должен быть не меньше 4 символов") ]
-    [MaxLength(4, ErrorMessage = "PIN-код не должен превышать 4 символов")]
+    [MaxLength(4, ErrorMessage = "PIN-код не должен превышать более 4 символов")]
     [RegularExpression(@"^\d+$", ErrorMessage = "PIN-код должен состоять только из цифр")]
     public required string PinCode { get; set; }
     
