0abe9978fc2f67b6e2f0c1b7b022325485f87430
ilnarkin861 <ilnarkino@yandex.ru>
2025-12-24 17:42:20 +0300

    Changed pin code regexp pattern

--- IlnarApp.Application/Models/UserRegisterRequest.cs
+++ IlnarApp.Application/Models/UserRegisterRequest.cs
@@ -15,7 +15,7 @@ public class UserRegisterRequest
     [Required(ErrorMessage = "PIN-код не должен быть пустым")]
     [MinLength(4, ErrorMessage = "PIN-код должен быть не меньше 4 символов") ]
     [MaxLength(4, ErrorMessage = "PIN-код не должен превышать 4 символов")]
-    [RegularExpression(@"^\d{4}$", ErrorMessage = "PIN-код должен состоять только из цифр")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "PIN-код должен состоять только из цифр")]
     public required string PinCode { get; set; }
     
 }
