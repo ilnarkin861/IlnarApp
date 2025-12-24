2f18667935402d80944f1a44bc3d9c2f12f40002
ilnarkin861 <ilnarkino@yandex.ru>
2025-12-24 17:59:13 +0300

    Added Authorize attribute

--- IlnarApp.Api/Controllers/BaseController.cs
+++ IlnarApp.Api/Controllers/BaseController.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IlnarApp.Api.Controllers;
 
+
+
 [Route("[controller]")]
+[Authorize]
 public class BaseController : ControllerBase
 {
 	
