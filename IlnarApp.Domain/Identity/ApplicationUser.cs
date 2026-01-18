4f6928e32748c758e8afd1a0c42654414818891e
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-18 17:28:01 +0300

    Removed pin code field

--- IlnarApp.Domain/Identity/ApplicationUser.cs
+++ IlnarApp.Domain/Identity/ApplicationUser.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace IlnarApp.Domain.Identity;
@@ -6,8 +5,4 @@ namespace IlnarApp.Domain.Identity;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
-	[MinLength(4, ErrorMessage = "PIN-код не должен быть меньше 4 символов")]
-	[MaxLength(4, ErrorMessage = "PIN-код не должен быть больше 4 символов")]
-	[RegularExpression(@"^\d{4}$", ErrorMessage = "PIN-код должен состоять только из цифр")]
-	public required string? PinCode { get; set; }
 }
