8b8fd5e5d2e91d92214a41b14d2c8a3e82dddf0b
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-18 17:29:40 +0300

    Makemigrations

---
+++ IlnarApp.Infrastructure/Migrations/20260118142149_RemovePinCodeField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlnarApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePinCodeField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PinCode",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PinCode",
+                table: "Users",
+                type: "character varying(4)",
+                maxLength: 4,
+                nullable: true);
+        }
+    }
+}
