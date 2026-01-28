4ab1c43af449e1ba57528b4f37f6e35824f930f2
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-28 18:13:35 +0300

    Make migrations

---
+++ IlnarApp.Infrastructure/Migrations/20260128145917_AddDeletedFields.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlnarApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Tag",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "NoteType",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Note",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Archive",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Tag");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "NoteType");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Note");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Archive");
+        }
+    }
+}
