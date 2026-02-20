385e1b97d2e0b699a634c9013cedc17023b0f806
ilnarkin861 <ilnarkino@yandex.ru>
2026-02-20 15:38:22 +0300

    Added json ignore

--- IlnarApp.Domain/Tag/Tag.cs
+++ IlnarApp.Domain/Tag/Tag.cs
@@ -6,6 +6,8 @@ using Note;
 public class Tag : Entity
 {
 	public required string Title { get; set; }
+	
+	[Newtonsoft.Json.JsonIgnore]
 	public List<Note> Notes { get; set; } = [];
 	public DateTime CreatedAt { get; set; } = DateTime.UtcNow.AddHours(3);
 	public bool Deleted { get; set; }
