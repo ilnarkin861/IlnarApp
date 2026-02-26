b2a4d221e2ea96b7f5d7dc176028c239eecec052
ilnarkin861 <ilnarkino@yandex.ru>
2026-02-26 07:56:27 +0300

    Set json ignore attribute

--- IlnarApp.Domain/Archive/Archive.cs
+++ IlnarApp.Domain/Archive/Archive.cs
@@ -6,6 +6,8 @@ using Note;
 public class Archive : Entity
 {
 	public required string Title { get; set; }
+	
+	[Newtonsoft.Json.JsonIgnore]
 	public List<Note> Notes { get; set; } = [];
 	public DateTime CreatedAt { get; set; } = DateTime.UtcNow.AddHours(3);
 	public bool Deleted { get; set; }
