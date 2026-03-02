99e7855ce5ea3a2aff839a4bf662f972e5e87286
ilnarkin861 <ilnarkino@yandex.ru>
2026-03-02 19:14:49 +0300

    Added created at field

--- IlnarApp.Domain/Note/NoteType.cs
+++ IlnarApp.Domain/Note/NoteType.cs
@@ -5,4 +5,5 @@ public class NoteType : Entity
 {
 	public required string Title { get; set; }
 	public bool Deleted { get; set; }
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow.AddHours(3);
 }
---
+++ IlnarApp.Infrastructure/Migrations/20260302160053_AddedCreatedAtField.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlnarApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCreatedAtField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Note",
+                newName: "CreatedAt");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "NoteType",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "NoteType");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Note",
+                newName: "CreatedDate");
+        }
+    }
+}
