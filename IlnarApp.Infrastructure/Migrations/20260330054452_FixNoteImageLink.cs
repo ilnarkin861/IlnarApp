c1c6087ad02d99ff97c4eaf13db2682a6ed70a2e
ilnarkin861 <ilnarkino@yandex.ru>
2026-03-30 09:18:19 +0300

    Re init note image model

---
+++ IlnarApp.Infrastructure/Migrations/20260330054452_FixNoteImageLink.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlnarApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNoteImageLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NoteNoteImage_NoteImage_NoteImagesId",
+                table: "NoteNoteImage");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_NoteNoteImage_Note_NotesId",
+                table: "NoteNoteImage");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NoteNoteImage",
+                table: "NoteNoteImage");
+
+            migrationBuilder.RenameTable(
+                name: "NoteNoteImage",
+                newName: "NoteNoteImages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_NoteNoteImage_NotesId",
+                table: "NoteNoteImages",
+                newName: "IX_NoteNoteImages_NotesId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NoteNoteImages",
+                table: "NoteNoteImages",
+                columns: new[] { "NoteImagesId", "NotesId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NoteNoteImages_NoteImage_NoteImagesId",
+                table: "NoteNoteImages",
+                column: "NoteImagesId",
+                principalTable: "NoteImage",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NoteNoteImages_Note_NotesId",
+                table: "NoteNoteImages",
+                column: "NotesId",
+                principalTable: "Note",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NoteNoteImages_NoteImage_NoteImagesId",
+                table: "NoteNoteImages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_NoteNoteImages_Note_NotesId",
+                table: "NoteNoteImages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NoteNoteImages",
+                table: "NoteNoteImages");
+
+            migrationBuilder.RenameTable(
+                name: "NoteNoteImages",
+                newName: "NoteNoteImage");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_NoteNoteImages_NotesId",
+                table: "NoteNoteImage",
+                newName: "IX_NoteNoteImage_NotesId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NoteNoteImage",
+                table: "NoteNoteImage",
+                columns: new[] { "NoteImagesId", "NotesId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NoteNoteImage_NoteImage_NoteImagesId",
+                table: "NoteNoteImage",
+                column: "NoteImagesId",
+                principalTable: "NoteImage",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NoteNoteImage_Note_NotesId",
+                table: "NoteNoteImage",
+                column: "NotesId",
+                principalTable: "Note",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ IlnarApp.Infrastructure/Migrations/20260330055528_RemovedNoteImageModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlnarApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedNoteImageModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NoteNoteImages");
+
+            migrationBuilder.DropTable(
+                name: "NoteImage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NoteImage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoteImage", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoteNoteImages",
+                columns: table => new
+                {
+                    NoteImagesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    NotesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoteNoteImages", x => new { x.NoteImagesId, x.NotesId });
+                    table.ForeignKey(
+                        name: "FK_NoteNoteImages_NoteImage_NoteImagesId",
+                        column: x => x.NoteImagesId,
+                        principalTable: "NoteImage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NoteNoteImages_Note_NotesId",
+                        column: x => x.NotesId,
+                        principalTable: "Note",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoteNoteImages_NotesId",
+                table: "NoteNoteImages",
+                column: "NotesId");
+        }
+    }
+}
